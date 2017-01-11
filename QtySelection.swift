9323da529ece2055fff809b8766ace03ebe5b19f
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2017-01-10 20:47:19 -0800

    QtySelector: Creating a custom view with initializers.

---
+++ QtySelection.swift
@@ -0,0 +1,21 @@
+//
+//  QtySelection.swift
+//  SimpList
+//
+//  Created by Suzannah Kirk-Daligcon on 1/10/17.
+//  Copyright © 2017 SK. All rights reserved.
+//
+
+import UIKit
+
+class QtySelection: UIStackView {
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+   
+}
