dafc32746309584f47040c0138969ffb7344047b
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2017-01-11 15:11:55 -0800

    ItemList: Design custom table cell. It's simple, will show the quantity image and have the item's description.

---
+++ ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  SimpList
+//
+//  Created by Suzannah Kirk-Daligcon on 1/11/17.
+//  Copyright © 2017 SK. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
