5388652928a284123214a84f0750c4d1aa6146b6
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2017-01-17 09:29:22 -0800

    re-integrated delete functionality of rows in the table with sections.

--- SimpList/ItemTableViewController.swift
+++ SimpList/ItemTableViewController.swift
@@ -14,6 +14,13 @@ class ItemTableViewController: UITableViewController {
     
     var sections: [Section] = SectionsData().getSectionsFromData()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return sections.count
     }
@@ -35,7 +42,7 @@ class ItemTableViewController: UITableViewController {
             fatalError("The dequeued cell is not an instance of ItemTableViewCell.")
         }
         
-        // Fetches the appropriate item and quantityImage for the data source layout.
+        // Fetches the appropriate item name and quantityImage for the data source layout.
         let qty1 = UIImage(named: "qty1")
         let qty2 = UIImage(named: "qty2")
         let qty3 = UIImage(named: "qty3")
@@ -73,35 +80,164 @@ class ItemTableViewController: UITableViewController {
         return cell
     }
     
+    // I THINK I WANT/NEED THIS...
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            sections[indexPath.section].items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
 }
 
 
 
-    
-/*
-    
+
+
+ 
+ /*
+ 
+ // I DON'T THINK I NEED THESE...
+ override func didReceiveMemoryWarning() {
+ super.didReceiveMemoryWarning()
+ // Dispose of any resources that can be recreated.
+ }
+ 
+ /*
+ // Override to support rearranging the table view.
+ override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+ 
+ }
+ */
+ 
+ /*
+ // Override to support conditional rearranging of the table view.
+ override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+ // Return false if you do not want the item to be re-orderable.
+ return true
+ }
+ */
+ 
+ 
+ 
+ 
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ super.prepare(for: segue, sender: sender)
+ 
+ switch(segue.identifier ?? "") {
+ 
+ case "AddItem":
+ os_log("Adding a new item.", log: OSLog.default, type: .debug)
+ 
+ case "ShowDetail":
+ guard let itemDetailViewController = segue.destination as? ItemViewController else {
+ fatalError("Unexpected destination: \(segue.destination)")
+ }
+ guard let selectedItemCell = sender as? ItemTableViewCell else {
+ fatalError("Unexpected sender: \(sender)")
+ }
+ guard let indexPath = tableView.indexPath(for: selectedItemCell) else {
+ fatalError("The selected cell is not being displayed by the table")
+ }
+ let selectedItem = items[indexPath.row]
+ itemDetailViewController.item = selectedItem
+ 
+ default:
+ fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+ }
+ }
+ 
+ 
+ 
+ 
+ //MARK: Actions
+ @IBAction func unwindToItemList(sender: UIStoryboardSegue) {
+ if let sourceViewController = sender.source as? ItemViewController, let item = sourceViewController.item {
+ if let selectedIndexPath = tableView.indexPathForSelectedRow {
+ // Update an existing item.
+ items[selectedIndexPath.row] = item
+ tableView.reloadRows(at: [selectedIndexPath], with: .none)
+ }
+ else {
+ // Add a new item.
+ let newIndexPath = IndexPath(row: items.count, section: 0) // will need to update this section once implementing different locations.
+ 
+ items.append(item)
+ tableView.insertRows(at: [newIndexPath], with: .automatic)
+ }
+ }
+ }
+ 
+ 
+ 
+ //MARK: Private Methods
+ private func loadInitialItems() {
+ guard let item1 = Item(name: "sweet italian creamer", quantity: 1, location: "1") else {
+ fatalError("Unable to instantiate item1")
+ }
+ guard let item2 = Item(name: "juice", quantity: 2, location: "1") else {
+ fatalError("Unable to instantiate item2")
+ }
+ guard let item3 = Item(name: "sour cream", quantity: 3, location: "1") else {
+ fatalError("Unable to instantiate item3")
+ }
+ guard let item4 = Item(name: "top ramen", quantity: 4, location: "1") else {
+ fatalError("Unable to instantiate item4")
+ }
+ guard let item5 = Item(name: "yogurt", quantity: 5, location: "1") else {
+ fatalError("Unable to instantiate item5")
+ }
+ guard let item6 = Item(name: "hormel snacks", quantity: 6, location: "1") else {
+ fatalError("Unable to instantiate item6")
+ }
+ 
+ items += [item1, item2, item3, item4, item5, item6]
+ }
+ 
+ */
+
+
+
+
+
+
+/* KEEPING A COPY FOR REFERENCE IN CASE SOMETHING DOESN'T WORK...
+ 
     //MARK: Properties
     var items = [Item]()
-    
-    
+ 
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+ 
         // Use the edit button item prvided by the table view controller.
         navigationItem.leftBarButtonItem = editButtonItem
-        
+ 
         // Load the initial data.
         loadInitialItems()
     }
-    
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    
-    
+ 
+ 
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1 // I'll be updating this when I implement more than one table section <<<<<<<<<<<
@@ -110,12 +246,12 @@ class ItemTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count // I'll be updating this, too <<<<<<<<<<
     }
-    
+ 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+ 
         // Table view cells are reused and will be dequeued using a cell identifier.
         let cellIdentifier = "ItemTableViewCell"
-        
+ 
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ItemTableViewCell else {
             fatalError("The dequeued cell is not an instance of ItemTableViewCell.")
         }
@@ -276,4 +412,13 @@ class ItemTableViewController: UITableViewController {
     }
 
 */
+
+
+
+
+
+
+
+
+
 
