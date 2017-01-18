fc47c1071dc45f3066ee6465dd03d915154b8a0e
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2017-01-17 22:10:10 -0800

    cleaned up section data--each section does not need a default item.

--- SectionsData.swift
+++ SectionsData.swift
@@ -12,19 +12,19 @@ class SectionsData {
     func getSectionsFromData() -> [Section] {
         var sectionsArray = [Section]()
         
-        let health = Section(title: "health/bath", objects: [Item(name: "garnier pure clean shampoo", quantity: 1, location: "health/bath")!])
+        let health = Section(title: "health/bath", objects: [])
         sectionsArray.append(health)
         
         let toys = Section(title: "toys/garden/home", objects: [Item(name: "look at shopkins", quantity: 1, location: "toys/garden/home")!])
         sectionsArray.append(toys)
         
-        let kitchen = Section(title: "kitchen/crafts", objects: [Item(name: "can opener", quantity: 1, location: "kitchen/crafts")!])
+        let kitchen = Section(title: "kitchen/crafts", objects: [])
         sectionsArray.append(kitchen)
         
         let rightGroc = Section(title: "right grocery", objects: [Item(name: "frosted mini wheats", quantity: 1, location: "right grocery")!, Item(name: "triscuits", quantity: 2, location: "right grocery")!, Item(name: "cookies", quantity: 1, location: "right grocery")!, Item(name: "org green beans", quantity: 2, location: "right grocery")!, Item(name: "soup", quantity: 3, location: "right grocery")!, Item(name: "top ramen", quantity: 4, location: "right grocery")!, Item(name: "tuna", quantity: 3, location: "right grocery")!, Item(name: "hash", quantity: 2, location: "right grocery")!, Item(name: "spam", quantity: 2, location: "right grocery")!])
         sectionsArray.append(rightGroc)
         
-        let rightRef = Section(title: "back right refrigerated", objects: [Item(name: "sweet italian creamer", quantity: 1, location: "back right refrigerated")!, Item(name: "juice", quantity: 1, location: "back right refrigerated")!, Item(name: "tillamook yogurt", quantity: 5, location: "back right refrigerated")!, Item(name: "sour cream", quantity: 1, location: "back right refrigerated")!])
+        let rightRef = Section(title: "back right refrigerated", objects: [Item(name: "sweet italian creamer", quantity: 1, location: "back right refrigerated")!, Item(name: "juice", quantity: 1, location: "back right refrigerated")!, Item(name: "tillamook yogurt", quantity: 5, location: "back right refrigerated")!, Item(name: "tillamook sour cream", quantity: 1, location: "back right refrigerated")!])
         sectionsArray.append(rightRef)
         
         let leftGroc = Section(title: "left grocery", objects: [Item(name: "gatorade pack", quantity: 1, location: "left grocery")!, Item(name: "chips", quantity: 1, location: "left grocery")!, Item(name: "cider pack", quantity: 1, location: "left grocery")!])
@@ -33,7 +33,7 @@ class SectionsData {
         let leftRef = Section(title: "back left refrigerated", objects: [Item(name: "hormel wraps", quantity: 5, location: "back left refrigerated")!, Item(name: "lunchables", quantity: 2, location: "back left refrigerated")!, Item(name: "natural lunch meat", quantity: 1, location: "back left refrigerated")!, Item(name: "bacon", quantity: 1, location: "back left refrigerated")!, Item(name: "organic meat", quantity: 1, location: "back left refrigerated")!])
         sectionsArray.append(leftRef)
         
-        let clothes = Section(title: "clothes/shoes", objects: [Item(name: "socks", quantity: 1, location: "clothes/shoes")!])
+        let clothes = Section(title: "clothes/shoes", objects: [])
         sectionsArray.append(clothes)
         
         let natural = Section(title: "natural section", objects: [Item(name: "org milk", quantity: 1, location: "natural section")!, Item(name: "onion dip mix", quantity: 1, location: "natural section")!, Item(name: "bulk chili powder", quantity: 1, location: "natural section")!, Item(name: "bulk chili flakes", quantity: 1, location: "natural section")!, Item(name: "bulk granola supplies", quantity: 1, location: "natural section")!, Item(name: "bread", quantity: 1, location: "natural section")!, Item(name: "bagels", quantity: 1, location: "natural section")!])
