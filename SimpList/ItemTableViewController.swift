e7105b5ba702257bcdd88066e30dc78a650b704a
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2017-01-25 00:23:51 -0800

    updated all of my color schemes.

--- SimpList/ItemTableViewController.swift
+++ SimpList/ItemTableViewController.swift
@@ -14,11 +14,43 @@ class ItemTableViewController: UITableViewController {
     
     var sections: [Section] = SectionsData().getSectionsFromData()
     
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Use the edit button item provided by the table view controller.
         navigationItem.leftBarButtonItem = editButtonItem
+        
+        self.tableView.backgroundColor = hexStringToUIColor(hex: "F84D59")
+//        self.tableView.backgroundColor = UIColor.clear
+//        self.tableView.superview?.backgroundColor = hexStringToUIColor(hex: "F84D59")
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = hexStringToUIColor(hex: "1E3446") // blue
+        header.textLabel?.backgroundColor = hexStringToUIColor(hex: "F84D59") // red
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -31,6 +63,13 @@ class ItemTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sections[section].heading
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView {
+        let header :UITableViewHeaderFooterView = UITableViewHeaderFooterView()
+        
+        header.contentView.tintColor = UIColor(red: 30.0/255.0, green: 52.0/255.0, blue: 70.0/255.0, alpha: 1)
+        return header
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -170,243 +209,3 @@ class ItemTableViewController: UITableViewController {
         }
     }
 }
-
- 
- /*
- 
- // I DON'T NEED THESE RIGHT NOW, keeping these duplicates separated in case I need to add them in for other functionality...
- override func didReceiveMemoryWarning() {
- super.didReceiveMemoryWarning()
- // Dispose of any resources that can be recreated.
- }
- 
- /*
- // Override to support rearranging the table view.
- override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
- 
- }
- */
- 
- /*
- // Override to support conditional rearranging of the table view.
- override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
- // Return false if you do not want the item to be re-orderable.
- return true
- }
- */
-
- */
-
-
-
-
-
-
-/* KEEPING A COPY FOR REFERENCE IN CASE SOMETHING DOESN'T WORK...
- 
-    //MARK: Properties
-    var items = [Item]()
- 
- 
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
- 
-        // Use the edit button item prvided by the table view controller.
-        navigationItem.leftBarButtonItem = editButtonItem
- 
-        // Load the initial data.
-        loadInitialItems()
-    }
- 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
- 
- 
-    // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1 // I'll be updating this when I implement more than one table section <<<<<<<<<<<
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count // I'll be updating this, too <<<<<<<<<<
-    }
- 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
- 
-        // Table view cells are reused and will be dequeued using a cell identifier.
-        let cellIdentifier = "ItemTableViewCell"
- 
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ItemTableViewCell else {
-            fatalError("The dequeued cell is not an instance of ItemTableViewCell.")
-        }
-        
-        // Fetches the appropriate item and quantityImage for the data source layout.
-        let qty1 = UIImage(named: "qty1")
-        let qty2 = UIImage(named: "qty2")
-        let qty3 = UIImage(named: "qty3")
-        let qty4 = UIImage(named: "qty4")
-        let qty5 = UIImage(named: "qty5")
-        let qty6 = UIImage(named: "qty6")
-        
-        let item = items[indexPath.row]
-        cell.itemNameLabel.text = item.name
-        
-        switch (item.quantity)
-        {
-        case 1:
-            cell.qtyImageView.image = qty1
-            
-        case 2:
-            cell.qtyImageView.image = qty2
-            
-        case 3:
-            cell.qtyImageView.image = qty3
-            
-        case 4:
-            cell.qtyImageView.image = qty4
-        
-        case 5:
-            cell.qtyImageView.image = qty5
-            
-        case 6:
-            cell.qtyImageView.image = qty6
-            
-        default:
-            print("This default case is actually impossible based on the code in Item.swift")
-        }
-
-        return cell
-    }
-    
-
-    
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    
-
-    
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            items.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    
-
-    
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        super.prepare(for: segue, sender: sender)
-        
-        switch(segue.identifier ?? "") {
-            
-        case "AddItem":
-            os_log("Adding a new item.", log: OSLog.default, type: .debug)
-            
-        case "ShowDetail":
-            guard let itemDetailViewController = segue.destination as? ItemViewController else {
-                fatalError("Unexpected destination: \(segue.destination)")
-            }
-            guard let selectedItemCell = sender as? ItemTableViewCell else {
-                fatalError("Unexpected sender: \(sender)")
-            }
-            guard let indexPath = tableView.indexPath(for: selectedItemCell) else {
-                fatalError("The selected cell is not being displayed by the table")
-            }
-            let selectedItem = items[indexPath.row]
-            itemDetailViewController.item = selectedItem
-            
-        default:
-            fatalError("Unexpected Segue Identifier; \(segue.identifier)")
-        }
-    }
-    
-    
-    
-    
-    //MARK: Actions
-    @IBAction func unwindToItemList(sender: UIStoryboardSegue) {
-        if let sourceViewController = sender.source as? ItemViewController, let item = sourceViewController.item {
-            if let selectedIndexPath = tableView.indexPathForSelectedRow {
-                // Update an existing item.
-                items[selectedIndexPath.row] = item
-                tableView.reloadRows(at: [selectedIndexPath], with: .none)
-            }
-            else {
-                // Add a new item.
-                let newIndexPath = IndexPath(row: items.count, section: 0) // will need to update this section once implementing different locations.
-            
-                items.append(item)
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
-            }
-        }
-    }
-    
-    
-    
-    //MARK: Private Methods
-    private func loadInitialItems() {
-        guard let item1 = Item(name: "sweet italian creamer", quantity: 1, location: "1") else {
-            fatalError("Unable to instantiate item1")
-        }
-        guard let item2 = Item(name: "juice", quantity: 2, location: "1") else {
-            fatalError("Unable to instantiate item2")
-        }
-        guard let item3 = Item(name: "sour cream", quantity: 3, location: "1") else {
-            fatalError("Unable to instantiate item3")
-        }
-        guard let item4 = Item(name: "top ramen", quantity: 4, location: "1") else {
-            fatalError("Unable to instantiate item4")
-        }
-        guard let item5 = Item(name: "yogurt", quantity: 5, location: "1") else {
-            fatalError("Unable to instantiate item5")
-        }
-        guard let item6 = Item(name: "hormel snacks", quantity: 6, location: "1") else {
-            fatalError("Unable to instantiate item6")
-        }
-        
-        items += [item1, item2, item3, item4, item5, item6]
-    }
-
-*/
-
-
-
-
-
-
-
-
-
-
